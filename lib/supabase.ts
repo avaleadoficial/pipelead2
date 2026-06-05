2f832bb60cc003ab374d582f1182dcf0de6a2931
avaleadoficial <avaleadoficial@gmail.com>
2026-06-05 00:57:59 -0300

    Create supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://jglpzymxzuduhixkodtd.supabase.co",
+  "sb_publishable_jtTqLVrQ4mGsbj1DV9XXJA_VV2OcTRg"
+);
