c4fdb282ab79086fc6f9ac8b834c1aade220a263
avaleadoficial <avaleadoficial@gmail.com>
2026-06-15 19:56:46 -0300

    Update mobile-nav.tsx

--- app/components/mobile/mobile-nav.tsx
+++ app/components/mobile/mobile-nav.tsx
@@ -1,9 +1,34 @@
 "use client";
 
+import Link from "next/link";
+
 export function MobileNav() {
   return (
-    <div>
-      Mobile Navigation
-    </div>
+    <nav
+      className="
+        fixed
+        bottom-0
+        left-0
+        right-0
+        bg-white
+        border-t
+        flex
+        justify-around
+        py-4
+        z-50
+      "
+    >
+      <Link href="/mobile/dashboard">
+        Dashboard
+      </Link>
+
+      <Link href="/mobile/projetos">
+        Projetos
+      </Link>
+
+      <Link href="/mobile/configuracoes">
+        Config
+      </Link>
+    </nav>
   );
 }
