08b83e1b22e984fd37d8e80cc26a9500f6643307
avaleadoficial <avaleadoficial@gmail.com>
2026-06-15 19:33:37 -0300

    Update layout.tsx

--- app/mobile/layout.tsx
+++ app/mobile/layout.tsx
@@ -1,1 +1,17 @@
+import { MobileNav } from "../components/mobile/mobile-nav";
 
+export default function MobileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <main className="min-h-screen pb-20">
+        {children}
+      </main>
+
+      <MobileNav />
+    </>
+  );
+}
