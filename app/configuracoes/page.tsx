d964b86736f8d049b27cfa5f204a44444a8605b4
avaleadoficial <avaleadoficial@gmail.com>
2026-06-17 18:06:13 -0300

    Update page.tsx

--- app/configuracoes/page.tsx
+++ app/configuracoes/page.tsx
@@ -179,7 +179,7 @@ export default function ConfiguracoesPage() {
   "
 >
 
-        <h1 className="text-5xl font-bold mb-10">
+        <h1 className="text-3xl font-bold mb-8">
           Configurações
         </h1>
 
